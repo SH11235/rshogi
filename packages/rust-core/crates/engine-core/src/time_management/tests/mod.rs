e2314b33d59eae0bebfc6d84857a2217103257df
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2025-09-28 18:10:40 +0900

    format

--- packages/rust-core/crates/engine-core/src/time_management/tests/mod.rs
+++ packages/rust-core/crates/engine-core/src/time_management/tests/mod.rs
@@ -1,5 +1,6 @@
 //! Time management module tests
 
+mod byoyomi_rounding_tests;
 mod byoyomi_tests;
 mod concurrent_tests;
 mod integration_tests;
@@ -7,6 +8,5 @@ mod phase_integration_tests;
 mod ponder_tests;
 mod rounding_tests;
 mod scheduled_stop_tests;
-mod byoyomi_rounding_tests;
 
 pub use crate::time_management::test_utils::{mock_advance_time, mock_set_time};
